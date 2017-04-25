0e7c8c06262f5b8e7cd5495adce682e68a2a5173
Amrik Sadhra <amriksadhra@gmail.com>
2017-04-26 00:28:24 +0100

    Cleanup

--- lcd_buffer.d
+++
@@ -1,4 +0,0 @@
-.\lcd_buffer.o: lcd_buffer.c
-.\lcd_buffer.o: lcd_buffer.h
-.\lcd_buffer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\lcd_buffer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
--- queue.d
+++
@@ -1,6 +0,0 @@
-.\queue.o: queue.c
-.\queue.o: queue.h
-.\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\queue.o: dmm_config.h
